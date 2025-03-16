adc9e4aea5c6f93d34bf9584dea5907dfa726aff
CB <chris.jhp.brown@protonmail.me>
2025-03-16 23:08:08 +1100

    attempted storyblok integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import { storyblok } from "@storyblok/astro";
 
 import cloudflare from "@astrojs/cloudflare";
 
@@ -11,11 +12,42 @@ import tailwindcss from "@tailwindcss/vite";
 
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
+import { loadEnv } from 'vite'
+
+import { apiPlugin, storyblokInit } from '@storyblok/js';
+
+const { storyblokApi } = storyblokInit({
+  accessToken: 'wJsAtBar4zDNeUJfurrjmAtt',
+  apiOptions: {
+    // storyblok-js-client config object
+    cache: { type: 'memory' },
+  },
+  use: [apiPlugin],
+});
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK')
+
 // https://astro.build/config
 
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), storyblok({
+
+      accessToken: "wJsAtBar4zDNeUJfurrjmAtt",
+
+      components: {
+
+        page: 'storyblok/Page',
+    
+        feature: 'storyblok/Feature',
+    
+        grid: 'storyblok/Grid',
+    
+        teaser: 'storyblok/Teaser',
+    
+      },
+      })
+  ],
 
   adapter: cloudflare({
     platformProxy: {
@@ -26,6 +58,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss(), basicSsl()],
     server: {
+      // @ts-ignore
       https: true,
     }
   }
