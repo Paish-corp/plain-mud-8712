989004b4df1e5d38fe398ac1a5a504b3ddaac22c
CB <chris.jhp.brown@protonmail.me>
2025-03-16 19:57:22 +1100

    Refactor configuration and update dependencies for improved site functionality and branding

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,28 @@ import sitemap from "@astrojs/sitemap";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
 // https://astro.build/config
+
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
+
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
     },
   }),
+
+  vite: {
+    plugins: [tailwindcss(), basicSsl()],
+    server: {
+      https: true,
+    }
+  }
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Paish Coporation";
-export const SITE_NAME = "Paish Coporation";
-export const SITE_URL = "https://paishcorp.com";
+export const SITE_TITLE = "Paish Corporation";
+export const SITE_NAME = "Paish Corporation";
+export const SITE_URL = "https://www.paishcorp.com";
 export const SITE_IMAGE = "/images/logo.png";
-export const SITE_KEYWORDS = "paish, coporation, paishcorp, paishcorp.com";
+export const SITE_KEYWORDS = "paish, corporation, paishcorp, paishcorp.com";
 export const SITE_DESCRIPTION = "Paish Corporation is a music development agency, specializing in creating high-quality culture for our clients.";
 
